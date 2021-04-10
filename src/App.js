342dd3126250918aaacee2c98fcd2e81539a45fa
Anik Thomas <aniek.sw@gmail.com>
2021-04-10 13:45:25 -0600

    React Week-2 Workshop Assignment

--- src/App.js
+++ src/App.js
@@ -25,6 +25,7 @@ class App extends Component {
         );
       }
     } 
+    
 
 
 export default App;
--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,38 +1,25 @@
 import React, {Component} from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
 
-class Directory extends Component{
+class Directory extends Component {
     constructor(props){
         super(props);
             this.state = {
                 selectedCampsite: null
             };    
     }
-    onCampSiteSelect(campsite){
+    onCampsiteSelect(campsite){
         this.setState ({
             selectedCampsite: campsite
+            
         });
     }
-
-    renderSelectedCampsite(campsite){
-        if (campsite) {
-            return (
-                <Card>
-                    <CardImg top src={campsite.image} alt={campsite.name} />
-                    <CardBody>
-                        <CardTitle>{campsite.name}</CardTitle>
-                        <CardText>{campsite.description}</CardText>
-                    </CardBody>
-                </Card>
-            );
-        }
-        return <div/>;
-    }
     render(){
-        const directory = this.props.campsites.map(campsite=> {
+        const directory = this.props.campsites.map(campsite => {
             return(
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={()=>this.onCampSiteSelect(campsite)}>
+                    <Card onClick={()=>this.onCampsiteSelect(campsite)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name}/>
                         <CardImgOverlay>
                            <CardTitle><h2>{campsite.name}</h2></CardTitle> 
@@ -43,12 +30,10 @@ class Directory extends Component{
         });
         return(
             <div className="container">
-                <div className="row">{directory}</div>
                 <div className="row">
-                    <div className="col-md-5 m-1">
-                        {this.renderSelectedCampsite(this.state.selectedCampsite)}
-                    </div> 
+                    {directory}
                 </div>
+                <CampsiteInfo campsite={this.state.selectedCampsite}/>
             </div>
         );
     }
